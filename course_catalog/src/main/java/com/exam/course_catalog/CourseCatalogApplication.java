46b391c591825074497578893690eba8b39f2b7a
Tharindu Akalaka <tharindu.thisun123@gmail.com>
2021-01-10 01:06:05 +0530

    eureka server setup

--- course_catalog/src/main/java/com/exam/course_catalog/CourseCatalogApplication.java
+++ course_catalog/src/main/java/com/exam/course_catalog/CourseCatalogApplication.java
@@ -2,8 +2,10 @@ package com.exam.course_catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CourseCatalogApplication {
 
 	public static void main(String[] args) {
